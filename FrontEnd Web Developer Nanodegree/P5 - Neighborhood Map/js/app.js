51610fcc8baa3d3fe2c5ed42643c9b6ea63d89b4
Manish Bisht <manish.bisht490@gmail.com>
2016-11-15 13:01:41 +0530

    Adding content using new operator

--- FrontEnd Web Developer Nanodegree/P5 - Neighborhood Map/js/app.js
+++ FrontEnd Web Developer Nanodegree/P5 - Neighborhood Map/js/app.js
@@ -10,120 +10,6 @@ $('.search-field').mousedown(function () {
     $(this).focus();
 });
 
-// Content of all the locations
-var markers = [
-    {
-        title: "Bhaskar Vidhya Ashram",
-        subtitle: "Private School",
-        latitude: 26.9053803,
-        longitude: 75.7259351,
-        streetAddress: "Lalarpura Road, Gandhi Path, Maa Karni Nagar",
-        cityAddress: "Jaipur, Rajasthan, IN",
-        url: "NA",
-        mobileNumber: "+91-9414336040",
-        show : true
-    },
-    {
-        title: "Hotel Chhavi Holidays",
-        subtitle: "Hotel",
-        latitude: 26.9055311,
-        longitude: 75.728137,
-        streetAddress: "Plot No. 11/12, Vivek Vihar, Gandhi Path (W)",
-        cityAddress: "Jaipur, Rajasthan, IN",
-        url: "NA",
-        mobileNumber: "0141-2471972",
-        show : true
-    },
-    {
-        title: "Handi",
-        subtitle: "Restaurant",
-        latitude: 26.906990,
-        longitude: 75.742848,
-        streetAddress: "18, Gautam Marg, Vaishali Nagar, Nemi Nagar",
-        cityAddress: "Jaipur, Rajasthan, IN",
-        url: "http://handirestaurant.com/",
-        mobileNumber: "0141-4016200",
-        show : true
-    },
-    {
-        title: "INOX - Amrapali",
-        subtitle: "Movie Theater",
-        latitude: 26.912631,
-        longitude: 75.743389,
-        streetAddress: "C-1, Vaibhav Complex, C Block, Amrapali Circle",
-        cityAddress: "Jaipur, Rajasthan, IN",
-        url: "http://www.inoxmovies.com/",
-        mobileNumber: "0141-5114482",
-        show : true
-    },
-    {
-        title: "Blue Dart",
-        subtitle: "Courier Service",
-        latitude: 26.911103,
-        longitude: 75.738878,
-        streetAddress: "Vaishali Tower, Vaishali Nagar",
-        cityAddress: "Jaipur, Rajasthan, IN",
-        url: "https://www.bluedart.com/",
-        mobileNumber: "0141-5105898",
-        show : true
-    },
-    {
-        title: "Hotel Seven Seas",
-        subtitle: "3-Star Hotel",
-        latitude: 26.906069,
-        longitude: 75.739583,
-        streetAddress: "A-6, Nemi Nagar, Gandhi Path",
-        cityAddress: "Jaipur, Rajasthan, IN",
-        url: "http://www.hotelsevenseasjaipur.com/",
-        mobileNumber: "0141-5108030",
-        show : true
-    },
-    {
-        title: "Global Heart & General Hospital",
-        subtitle: "Hospital",
-        latitude: 26.905506,
-        longitude: 75.738762,
-        streetAddress: "C1/27, Opposite Bharat Apartment",
-        cityAddress: "Jaipur, Rajasthan, IN",
-        url: "http://heartandgeneralhospital.com/",
-        mobileNumber: "0141-2440629",
-        show : true
-    },
-    {
-        title: "Shri Swaminarayan Mandir",
-        subtitle: "Hindu Temple",
-        latitude: 26.902167,
-        longitude: 75.740999,
-        streetAddress: "Sector 9, Chitrakoot",
-        cityAddress: "Jaipur, Rajasthan, IN",
-        url: "http://www.baps.org/",
-        mobileNumber: "0141-2246100",
-        show : true
-    },
-    {
-        title: "Pratap Marriage Garden",
-        subtitle: "Banquet Hall",
-        latitude: 26.906464,
-        longitude: 75.732889,
-        streetAddress: "Arpit Nagar, B Block, Vaishali Nagar",
-        cityAddress: "Jaipur, Rajasthan, IN",
-        url: "NA",
-        mobileNumber: "NA",
-        show : true
-    },
-    {
-        title: "ICICI Bank",
-        subtitle: "Bank",
-        latitude: 26.913179,
-        longitude: 75.743447,
-        streetAddress: "Lalarpura Road, Gandhi Path, Maa Karni Nagar",
-        cityAddress: "Jaipur, Rajasthan, IN",
-        url: "https://www.icicibank.com",
-        mobileNumber: "0141-3366777",
-        show : true
-    }
-];
-
 // Initialize the map and its content
 function loadMap() {
     var mapview = document.getElementById('map');
@@ -187,22 +73,24 @@ function AppViewModel() {
         });
         //self.infoWindow = new google.maps.InfoWindow({});
     }
+
+    // Content of all the locations
     self.markers = ko.observableArray([
-        new self.marker(),
-        new self.marker(),
-        new self.marker(),
-        new self.marker(),
-        new self.marker(),
-        new self.marker(),
-        new self.marker(),
-        new self.marker(),
-        new self.marker(),
-        new self.marker()
+        new self.marker("Bhaskar Vidhya Ashram", "Private School", 26.9053803, 75.7259351, "Lalarpura Road, Gandhi Path, Maa Karni Nagar", "Jaipur, Rajasthan, IN", "NA", "+91-9414336040"),
+        new self.marker("Hotel Chhavi Holidays", "Hotel", 26.9055311, 75.728137, "Plot No. 11/12, Vivek Vihar, Gandhi Path (W)", "Jaipur, Rajasthan, IN", "NA", "0141-2471972"),
+        new self.marker("Handi", "Restaurant", 26.906990, 75.742848, "18, Gautam Marg, Vaishali Nagar, Nemi Nagar", "Jaipur, Rajasthan, IN", "http://handirestaurant.com/", "0141-4016200"),
+        new self.marker("INOX - Amrapali", "Movie Theater", 26.912631, 75.743389, "C-1, Vaibhav Complex, C Block, Amrapali Circle", "Jaipur, Rajasthan, IN", "http://www.inoxmovies.com/", "0141-5114482"),
+        new self.marker("Blue Dart", "Courier Service", 26.911103, 75.738878, "Vaishali Tower, Vaishali Nagar", "Jaipur, Rajasthan, IN", "https://www.bluedart.com/", "0141-5105898"),
+        new self.marker("Hotel Seven Seas", "3-Star Hotel", 26.906069, 75.739583, "A-6, Nemi Nagar, Gandhi Path", "Jaipur, Rajasthan, IN", "http://www.hotelsevenseasjaipur.com/", "0141-5108030"),
+        new self.marker("Global Heart & General Hospital", "Hospital", 26.905506, 75.738762, "C1/27, Opposite Bharat Apartment", "Jaipur, Rajasthan, IN", "http://heartandgeneralhospital.com/", "0141-2440629"),
+        new self.marker("Shri Swaminarayan Mandir", "Hindu Temple", 26.902167, 75.740999, "Sector 9, Chitrakoot", "Jaipur, Rajasthan, IN", "http://www.baps.org/", "0141-2246100"),
+        new self.marker("Pratap Marriage Garden", "Banquet Hall", 26.906464, 75.732889, "Arpit Nagar, B Block, Vaishali Nagar", "Jaipur, Rajasthan, IN", "NA", "NA"),
+        new self.marker("ICICI Bank", "Bank", 26.913179, 75.743447, "Lalarpura Road, Gandhi Path, Maa Karni Nagar", "Jaipur, Rajasthan, IN", "https://www.icicibank.com", "0141-3366777")
     ]);
     self.query = ko.observable("");
-    self.showMarkers = ko.computed(function() {
-        return ko.utils.arrayFilter(self.markers(), function(marker) {
-            if(marker.title.toLowerCase().indexOf(self.query().toLowerCase()) >= 0){
+    self.showMarkers = ko.computed(function () {
+        return ko.utils.arrayFilter(self.markers(), function (marker) {
+            if (marker.title.toLowerCase().indexOf(self.query().toLowerCase()) >= 0) {
                 marker.show = true
             }
             else {
@@ -211,18 +99,18 @@ function AppViewModel() {
         });
         //self.markers.removeAll();
         /*for(var x in markers) {
-            if(markers[x].title.toLowerCase().indexOf(self.query().toLowerCase()) >= 0) {
-                markers[x].display = true;
-                //self.markers.push(markers[x]);
-            }
-            else {
-                markers[x].display = false;
-            }
-        }*/
+         if(markers[x].title.toLowerCase().indexOf(self.query().toLowerCase()) >= 0) {
+         markers[x].display = true;
+         //self.markers.push(markers[x]);
+         }
+         else {
+         markers[x].display = false;
+         }
+         }*/
     }, self);
     self.showMarkers.subscribe(function () {
-        for(var i=0; i<markers.length;i++){
-            if(markers[i].check==false){
+        for (var i = 0; i < markers.length; i++) {
+            if (markers[i].check == false) {
                 self.marker.setVisible(false);
             }
         }
