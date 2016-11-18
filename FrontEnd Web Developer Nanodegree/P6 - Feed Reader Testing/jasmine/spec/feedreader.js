ff39353a633f1457bc2cd0f2b4615775f718a436
Manish Bisht <manish.bisht490@gmail.com>
2016-11-18 09:04:55 +0530

    beforeEach for "The menu" written

--- FrontEnd Web Developer Nanodegree/P6 - Feed Reader Testing/jasmine/spec/feedreader.js
+++ FrontEnd Web Developer Nanodegree/P6 - Feed Reader Testing/jasmine/spec/feedreader.js
@@ -51,7 +51,11 @@ $(function() {
 
     /* TODO: Write a new test suite named "The menu" */
     describe('The menu', function () {
-
+        var menu;
+        beforeEach(function () {
+            menu = $('.menu-icon-link');
+        });
+        
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
@@ -64,7 +68,7 @@ $(function() {
           * clicked and does it hide when clicked again.
           */
     });
-    
+
     /* TODO: Write a new test suite named "Initial Entries" */
 
         /* TODO: Write a test that ensures when the loadFeed
