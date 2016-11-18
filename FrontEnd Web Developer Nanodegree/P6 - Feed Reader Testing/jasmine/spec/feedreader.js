c7bc822117c4a775ec08ece4fc1b2d6619af3303
Manish Bisht <manish.bisht490@gmail.com>
2016-11-18 09:10:52 +0530

    test suite named "Initial Entries" described

--- FrontEnd Web Developer Nanodegree/P6 - Feed Reader Testing/jasmine/spec/feedreader.js
+++ FrontEnd Web Developer Nanodegree/P6 - Feed Reader Testing/jasmine/spec/feedreader.js
@@ -79,13 +79,14 @@ $(function() {
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
-
+    describe('Initial Entries', function () {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
         /* TODO: Write a test that ensures when a new feed is loaded
