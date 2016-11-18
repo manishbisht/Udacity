cdc0aace21864f236c0a72527f03ffbf210a86aa
Manish Bisht <manish.bisht490@gmail.com>
2016-11-18 16:43:00 +0530

    beforeEach for "New Feed Selection" written

--- FrontEnd Web Developer Nanodegree/P6 - Feed Reader Testing/jasmine/spec/feedreader.js
+++ FrontEnd Web Developer Nanodegree/P6 - Feed Reader Testing/jasmine/spec/feedreader.js
@@ -101,7 +101,13 @@ $(function() {
 
     /* TODO: Write a new test suite named "New Feed Selection" */
     describe('New Feed Selection', function () {
-
+        var feeds;
+        beforeEach(function (done) {
+            loadFeed(0, function () {
+                feeds = $('.feed').html();
+                done();
+            });
+        });
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
