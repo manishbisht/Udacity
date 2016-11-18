cd399204975b56736458a4190f53267d1d0bd2c2
Manish Bisht <manish.bisht490@gmail.com>
2016-11-18 09:03:29 +0530

    "The menu" described

--- FrontEnd Web Developer Nanodegree/P6 - Feed Reader Testing/jasmine/spec/feedreader.js
+++ FrontEnd Web Developer Nanodegree/P6 - Feed Reader Testing/jasmine/spec/feedreader.js
@@ -50,7 +50,8 @@ $(function() {
     });
 
     /* TODO: Write a new test suite named "The menu" */
-        
+    describe('The menu', function () {
+
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
@@ -62,7 +63,8 @@ $(function() {
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
-
+    });
+    
     /* TODO: Write a new test suite named "Initial Entries" */
 
         /* TODO: Write a test that ensures when the loadFeed
