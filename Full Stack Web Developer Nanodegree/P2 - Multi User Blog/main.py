f040a25454ddbe54bc2a1538cbeae378a59f4616
Manish Bisht <manish.bisht490@gmail.com>
2016-11-28 23:05:30 +0530

    added jinja2

--- Full Stack Web Developer Nanodegree/P2 - Multi User Blog/main.py
+++ Full Stack Web Developer Nanodegree/P2 - Multi User Blog/main.py
@@ -14,9 +14,26 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import os
 import webapp2
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
+                               autoescape=True)
+
 
 class MainHandler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t=jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
     def get(self):
         self.response.write('Hello world!')
 
