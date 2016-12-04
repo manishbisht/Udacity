913cae23f2322004a8cf359c2242ad23bc073d9a
Manish Bisht <manish.bisht490@gmail.com>
2016-12-04 23:15:24 +0530

    updated styles of create template

--- Full Stack Web Developer Nanodegree/P2 - Multi User Blog/main.py
+++ Full Stack Web Developer Nanodegree/P2 - Multi User Blog/main.py
@@ -85,13 +85,9 @@ class Post(db.Model):
     created = db.DateTimeProperty(auto_now_add=True)
     last_modified = db.DateTimeProperty(auto_now=True)
 
-    def render_str(self, template, **params):
-        t = jinja_env.get_template(template)
-        return t.render(params)
-
     def render(self):
         self._render_text = self.content.replace('\n', '<br>')
-        return self.render_str("post.html", p=self)
+        return render_str("post.html", p=self)
 
 
 class MainHandler(BlogHandler):
