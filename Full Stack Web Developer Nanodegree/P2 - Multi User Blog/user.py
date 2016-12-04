2e0b8dba5a1d014f80373999e373768ce5d7e5ff
Manish Bisht <manish.bisht490@gmail.com>
2016-12-04 22:55:09 +0530

    completed user entity

--- Full Stack Web Developer Nanodegree/P2 - Multi User Blog/user.py
+++ Full Stack Web Developer Nanodegree/P2 - Multi User Blog/user.py
@@ -32,4 +32,20 @@ class User(db.Model):
 
     @classmethod
     def by_id(cls, uid):
-        return User.get_by_id(uid, parent = users_key())
+        return User.get_by_id(uid, parent=users_key())
+
+    @classmethod
+    def by_name(cls, name):
+        return User.all().filter('name =', name).get()
+
+    @classmethod
+    def register(cls, name, pw, email=None):
+        pw_hash = make_pw_hash(name, pw)
+        return User(parent=users_key(), name=name, pw_hash=pw_hash,
+                    email=email)
+
+    @classmethod
+    def login(cls, name, pw):
+        u = cls.by_name(name)
+        if u and valid_pw(name, pw, u.pw_hash):
+            return u
